90403ce162613b8aa5d5b615dadbd9f01be2fdd1
Onni Salminen <o.j.salminen@gmail.com>
2019-12-15 13:18:45 +0200

    kinda works now but not really, it places the pieces to wrong places

--- map.c
+++ map.c
@@ -6,7 +6,7 @@
 /*   By: osalmine <osalmine@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/12/15 09:42:09 by osalmine          #+#    #+#             */
-/*   Updated: 2019/12/15 13:08:36 by osalmine         ###   ########.fr       */
+/*   Updated: 2019/12/15 13:18:02 by osalmine         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,18 +48,19 @@ void	place_piece(t_map *map, t_etri *tetris, t_point *start, char c)
 		i = 0;
 		while (i < tetris->height)
 		{
+			printf("\n---\nplace_piece\n");
 			printf("\ntetris->arr[i][j]: %c\n", tetris->arr[i][j]);
 			printf("start->y: %d, start->x: %d\n", start->y, start->x);
 			printf("i: %d, j: %d\n", i, j);
 			printf("start->y + i: %d, start->x + j: %d\n", start->y + i, start->x + j);
-			printf("map->map[start->y + i][start->x + j]: %c\n", map->map[start->y + i][start->x + j]);
+			printf("map->map[start->y + i][start->x + j]: %c\n---\n", map->map[start->y + i][start->x + j]);
 			if (tetris->arr[i][j] == '#')
 				map->map[start->y + i][start->x + j] = c;
 			i++;
 		}
 		j++;
 	}
-	ft_memdel((void**)&start);
+//	ft_memdel((void**)&start);
 }
 
 /*int     check(t_map *map, t_etri *piece, int curx, int cury)
@@ -104,8 +105,9 @@ int		check_map_spot(t_map *map, t_etri *tetris, t_point *start)
 		x = 0;
 		while (x < tetris->width)
 		{
-//			printf("tetris char: %c\n", tetris->arr[y][x]);
-//			printf("map char: %c\n", map->map[start->y + y][start->x + x]);
+			printf("\n---\ncheck_map_spot\n");
+			printf("tetris char: %c\n", tetris->arr[y][x]);
+			printf("map char: %c\n---\n", map->map[start->y + y][start->x + x]);
 			if (tetris->arr[y][x] == '#' && map->map[start->y + y][start->x + x] != '.')
 				return (0);
 			x++;
